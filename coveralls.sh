9e3c003b27cc27d85b59828ac81be29eb2adbac6
Piotr Jagielski <pjagielski@o2.pl>
2014-10-27 20:37:51 +0100

    travis + coveralls fix

--- coveralls.sh
+++ coveralls.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-lein cloverage -o cov --coveralls
+COVERALLS_URL='https://coveralls.io/api/v1/jobs'
+lein cloverage -o target/coveralls --coveralls
+curl -F 'json_file=@target/coveralls/coveralls.json' "$COVERALLS_URL"
